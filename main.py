6165cc31669c464db977e5dbe17ce616dea3673d
jennettej <jennettej@uncw.edu>
2026-03-10 12:34:37 -0400

    Create main.py
    Initial commit for main

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+SENG 201 - Term Project
+Starter Script
+
+Description:
+    TODO: Describe what this program/module is supposed to do.
+"""
+
+
+def main():
+    """
+    Main entry point for the program.
+    """
+    # TODO: Implement program logic here
+    pass
+
+
+if __name__ == "__main__":
+    main()
