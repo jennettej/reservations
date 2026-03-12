505023b7272627a3756e0d6720676db71692da07
jovanirh7@gmail.com <jovanirh7@gmail.com>
2026-03-12 18:49:35 -0400

    created function where user is prompted to create an account (username, password)

--- main.py
+++ main.py
@@ -7,12 +7,26 @@ Description:
 """
 
 
+FILE_NAME = "accounts.txt"
+
+def create_account():
+
+    print("\nWelcome to our hotel system\n\n\n")
+    print("Please create an account\n")
+    username = input("Please enter your username: ").strip()
+    password = input("Please enter your password: ").strip()
+
+    with open(FILE_NAME, "a") as file:
+        file.write(username + "," + password + "\n")
+
+    print("Account Created Successfully")
+
 def main():
     """
     Main entry point for the program.
     """
     # TODO: Implement program logic here
-    pass
+    create_account()
 
 
 if __name__ == "__main__":
