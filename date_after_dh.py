e2e58f8e71332e9ac014d166ede682c97c63111b
Holladay Industries <HolladayIndustries@protonmail.com>
2026-03-18 10:16:21 -0400

    Create function and logic for calculating date after given date and time

---
+++ date_after_dh.py
@@ -0,0 +1,14 @@
+# Take input, start date and duration in days, return datetime.date object
+# Started by David Holladay on March 18th, 2026
+import datetime
+
+def date_after(start_date: str, duration_days: int):
+    '''
+Takes a string input as a date, format MM/DD/YYYY, and the number of days, as a int. Returns datetime object
+    '''
+    
+    start_date = datetime.datetime.strptime(start_date, "%m/%d/%Y")
+
+    end_date = start_date + datetime.timedelta(days=duration_days)
+
+    return end_date
