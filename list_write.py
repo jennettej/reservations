5152a4d97cc63d72ce2e60757de778fe4855d182
dek8486 <dekelleher2022@gmail.com>
2026-04-09 08:25:35 -0400

    Still WIP write to list file

--- list_write.py
+++ list_write.py
@@ -1,10 +1,16 @@
 #try:
 f1 = open("reservations.json", "r")
+res = "test"
 res = str(f1.readlines())
 print("Things to add: "+res)
 f2 = open("list.txt", "a")
 f2.write(res)
-f1.close(), f2.close()
+f1.close()
+f2.close()
+print("Reservation successfully written")
+f3 = open("list.txt","r")
+print(str(f3.readlines()))
+f3.close()
 #except:
 #       print("Error: no existing file to write to")
 
