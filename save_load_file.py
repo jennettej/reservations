97c40f1b5b9c466b3958dc312ab9e39f2b34da61
dek8486 <dekelleher2022@gmail.com>
2026-04-30 14:21:25 -0400

    save_load_file should be working perfectly now

--- save_load_file.py
+++ save_load_file.py
@@ -4,7 +4,7 @@ import os #https://www.w3schools.com/python/python_file_remove.asp
 def getInput():
 	'''getInput: gets a bunch of inputs from the user and writes them to a file'''
 	try: #if a file already exists
-	  f = open("reservations.json","w") #open it
+	  f = open("reservations.json","a") #open it
 	except: #if not, do nothing
 	  print("No file exists, creating...")
 	  f = open("reservations.json","x") #make a new file
@@ -21,6 +21,10 @@ def getInput():
 	y = json.dumps(x) #convert the dictionary to json format
 	print(y)
 	f.write(y) #write the json to the file
+<<<<<<< Updated upstream
 	f.close() #close the file
 
 getInput()
+=======
+	f.close() #close the file
+>>>>>>> Stashed changes
