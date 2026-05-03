c1dfcaf0048a1fd08340fc0cac5b7ce789f7a82c
Braden Salter <salterwolf89@gmail.com>
2026-05-02 20:21:33 -0400

    added a trends function with part functionality so that it can find trends with the related dates

--- perf_report.py
+++ perf_report.py
@@ -1,4 +1,5 @@
 import json
+import datetime 
 
 def performance_report():
     '''reads the file with the current reservations and returns the number of reservations
@@ -8,6 +9,18 @@ def performance_report():
         res_file = json.load(filename)
 
     return len(res_file), res_file
+
+def date_trends():
+    with open('reservations.json', 'r') as filename:
+        lst = []
+        res_file = json.load(filename)
+        for reservations in res_file:
+            arrivalDate = datetime.datetime.strptime(reservations['arrivalDate'], '%m-%d-%Y').date()
+            leaveDate = datetime.datetime.strptime(reservations['leaveDate'], '%m-%d-%Y').date()
+            lst.append([arrivalDate, leaveDate, (leaveDate - arrivalDate).days])
+            
+            
+            
 
 def main():
     active, reservations = performance_report()
