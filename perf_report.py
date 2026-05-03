e7b4772bc0866795987a2fb3a13e0218ec14d9ee
Braden Salter <salterwolf89@gmail.com>
2026-05-02 22:21:32 -0400

    added frequency functionality, and ordered the reservation list, corrected issues on ordering, have the month reset if it passes 12

--- perf_report.py
+++ perf_report.py
@@ -13,13 +13,34 @@ def performance_report():
 def date_trends():
     with open('reservations.json', 'r') as filename:
         lst = []
+        freq_dic = {}
         res_file = json.load(filename)
+
         for reservations in res_file:
             arrivalDate = datetime.datetime.strptime(reservations['arrivalDate'], '%m-%d-%Y').date()
             leaveDate = datetime.datetime.strptime(reservations['leaveDate'], '%m-%d-%Y').date()
             lst.append([arrivalDate, leaveDate, (leaveDate - arrivalDate).days])
-            
-            
+        
+
+            curr_month = arrivalDate.month 
+            curr_year = arrivalDate.year 
+            lst_month = leaveDate.month
+            lst_year = leaveDate.year
+
+            while (curr_year, curr_month) <= (leaveDate.year, leaveDate.month):
+                freq_dic[f'{curr_month}-{curr_year}'] = freq_dic.get(f'{curr_month}-{curr_year}', 0) + 1
+                if curr_month == 12:
+                    curr_year += 1
+                    curr_month = 1
+
+                else:
+                    curr_month += 1
+    sorted_lst = sorted(lst, key=lambda x: x[0])
+    return sorted_lst, freq_dic
+
+
+
+
             
 
 def main():
@@ -27,6 +48,10 @@ def main():
     print(active)
     for i in reservations:
         print(i)
+    sorted_lst, freq_dic = date_trends()
+    print(sorted_lst)
+    print()
+    print(freq_dic)
 
 
 main()
